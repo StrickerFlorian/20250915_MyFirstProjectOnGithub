eb005a2b458cf65113f077a02ce3b61d64dca467
Florian Stricker <florianmattl1@gmail.com>
2025-09-16 11:31:00 +0200

    abc lg michi

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("Version #1");
 Console.WriteLine("add_something #1");
-
-
+Console.WriteLine("add_something #2");
+Console.WriteLine("add_something #3");
 Console.WriteLine("add_something #21");
